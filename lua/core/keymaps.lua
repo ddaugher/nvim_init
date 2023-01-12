423a63cb92449af5581bfa5a8acbedb6967d1eeb
DJ Daugherty <ddaugher@gmail.com>
2023-01-11 20:18:54 -0500

    updates to include keymap for pane navigation and resizing

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,4 @@
+local opts = { noremap = true, silent = true }
 -- [[ Basic Keymaps ]]
 -- Set <space> as the leader key
 -- See `:help mapleader`
@@ -10,6 +11,18 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+-- Use ctrl-[hjkl] to select the active split!
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", opts)
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", opts)
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", opts)
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", opts)
+
+--resize with arrows
+vim.api.nvim_set_keymap("n", "<A-Up>", ":resize +2<cr>", opts)
+vim.api.nvim_set_keymap("n", "<A-Down>", ":resize -2<cr>", opts)
+vim.api.nvim_set_keymap("n", "<A-Left>", ":vertical resize -2<cr>", opts)
+vim.api.nvim_set_keymap("n", "<A-Right>", ":vertical resize +2<cr>", opts)
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
